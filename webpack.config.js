9ae97b19b00bd298845d0ed2ab19cabfa3231629
srj-17 <saugatrijal17@gmail.com>
2024-08-16 12:54:00 +0545

    Add html-loader
    Firstly, to create a page, how it looks, purely in
    html. Will change that to javascript in the later
    commit

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,11 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
-            }
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
         ]
-    }
+    },
 };
