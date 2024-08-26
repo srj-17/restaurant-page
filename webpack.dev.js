31855f3c97a0d85eff79d6d2c643ebeea9d1935f
srj-17 <saugatrijal17@gmail.com>
2024-08-26 11:17:24 +0545

    Setup webpack.common.js
    Reduces duplication in webpack configuration for
    development and production

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,38 +1,10 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const { webpack } = require("webpack");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
+module.exports = merge(common, {
     mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-        template: "./src/template.html",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                use: ["html-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ]
-    },
-};
+})
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,38 +1,7 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const { webpack } = require("webpack");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
+module.exports = merge(common, {
     mode: "production",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-        template: "./src/template.html",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                use: ["html-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ]
-    },
-};
+    devtool: "source-map",
+})
